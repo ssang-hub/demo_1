493516567eba218d4c6950cd778662b39de7d7d0
ssang <nguyendinhsang15102001@gmail.com>
2022-02-09 19:26:36 +0700

    update

--- filetest.py
+++ filetest.py
@@ -1,11 +1,6 @@
-a = int(input())
-b = int(input())
-
-def power(a, b):
-    if b==1:
-        return a
-    elif b==0:
-        return 1
-    else:
-        return power(a,int( b/2))*power(a, b-int(b/2))
-print(power(a,b))
+a = input()
+arr=a.split()
+if int(arr[0])+int(arr[2])==int(arr[4]):
+    print("YES")
+else:
+    print("NO")
